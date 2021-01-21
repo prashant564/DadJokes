dfdf28021385bcd40b51262575f38e000907f3cf
Dprash <prashantdixit600@gmail.com>
2021-01-21 22:12:30 +0530

    UI changes

--- app/src/main/java/com/prashd/dadjokes/views/MainActivity.kt
+++ app/src/main/java/com/prashd/dadjokes/views/MainActivity.kt
@@ -1,19 +1,14 @@
 package com.prashd.dadjokes.views
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Typeface
 import android.os.Bundle
-import android.util.Log
+import android.text.method.ScrollingMovementMethod
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
-import androidx.lifecycle.lifecycleScope
-import com.prashd.dadjokes.R
 import com.prashd.dadjokes.databinding.ActivityMainBinding
-import com.prashd.dadjokes.network.RetrofitClientInstance
 import com.prashd.dadjokes.repository.JokeRepository
-import kotlinx.coroutines.Dispatchers
-import retrofit2.HttpException
-import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -21,18 +16,33 @@ class MainActivity : AppCompatActivity() {
         val factory = JokeViewModelFactory(JokeRepository())
         ViewModelProviders.of(this, factory).get(JokeViewModel::class.java)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val tf: Typeface = Typeface.createFromAsset(assets, "fonts/Raleway-Regular.ttf")
+        binding.tvTest.typeface = tf
+        binding.tvTest.movementMethod = ScrollingMovementMethod()
+
         viewmodel.randomJoke.observe(this, Observer {
-            binding.tvTest.text = it.joke
+            if (it == null) {
+                binding.progressBar.visibility = View.VISIBLE
+            } else {
+                binding.progressBar.visibility = View.GONE
+                binding.tvTest.text = it.joke
+            }
         })
 
         binding.btnGenerate.setOnClickListener {
             viewmodel.getRandomDadJoke()
+            binding.progressBar.visibility = View.VISIBLE
             viewmodel.randomJoke.observe(this, Observer {
-                binding.tvTest.text = it.joke
+                if (it != null) {
+                    binding.progressBar.visibility = View.GONE
+                    binding.tvTest.text = it.joke
+                }
             })
         }
     }
