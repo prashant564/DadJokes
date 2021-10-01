770e92b0dc286e008b6154622d29b179222b0e19
Swapnil Kusumwal <43251574+swapnilkusumwal@users.noreply.github.com>
2021-10-01 16:48:29 +0530

    feat: update Api integrations using Flows

--- app/src/main/java/com/prashd/dadjokes/repository/JokeRepository.kt
+++ app/src/main/java/com/prashd/dadjokes/repository/JokeRepository.kt
@@ -1,7 +1,16 @@
 package com.prashd.dadjokes.repository
 
+import com.prashd.dadjokes.network.JokeUI
 import com.prashd.dadjokes.network.RetrofitClientInstance
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import retrofit2.Response
 
 class JokeRepository {
-    suspend fun getDadJokeFromNetwork() = RetrofitClientInstance.api.getDadJokeFromNetwork("application/json")
+    fun getDadJokeFromNetwork(): Flow<Response<JokeUI>> = flow {
+        val jokesList = RetrofitClientInstance.api.getDadJokeFromNetwork("application/json")
+        emit(jokesList)
+    }.flowOn(Dispatchers.IO)
 }
